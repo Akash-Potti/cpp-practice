b16191be7029cb5134f3f6baaab5c10b2a7c1eff
SupAkash <akashpotti06@gmail.com>
2022-08-10 21:38:47 +0530

    formatting

--- Vowels/vowles.cpp
+++ Vowels/vowles.cpp
@@ -1,17 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     char string[100];
-    int i,vow=0;
-    cout<<"enter a string: "<<endl;
-    cin>>string;
-    for(int i=0;string[i];i++)
+    int i, vow = 0;
+    cout << "enter a string: " << endl;
+    cin >> string;
+    for (int i = 0; string[i]; i++)
     {
-        if(string[i]=='a'||string[i]=='e'||string[i]=='i'||string[i]=='o'||string[i]=='u')
+        if (string[i] == 'a' || string[i] == 'e' || string[i] == 'i' || string[i] == 'o' || string[i] == 'u')
         {
             vow++;
         }
     }
-    cout<<"The number of vowels are: "<<vow;
+
+    cout << "The number of vowels are: " << vow;
 }
